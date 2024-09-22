efa9dab4db031568b36eca71fb4bd5da190fa9d7
wxycherry <163128074+wxycherry@users.noreply.github.com>
2024-09-22 13:45:07 +0800

    删除工具类集合的路由及组件

--- src/router/route.ts
+++ src/router/route.ts
@@ -185,21 +185,6 @@ export const dynamicRoutes: Array<RouteRecordRaw> = [
 					icon: 'iconfont icon-gerenzhongxin',
 				},
 			},
-			{
-				path: '/tools',
-				name: 'tools',
-				component: () => import('/@/views/tools/index.vue'),
-				meta: {
-					title: 'message.router.tools',
-					isLink: '',
-					isHide: false,
-					isKeepAlive: true,
-					isAffix: false,
-					isIframe: false,
-					roles: ['admin', 'common'],
-					icon: 'iconfont icon-gongju',
-				},
-			},
 		],
 	},
 ];
