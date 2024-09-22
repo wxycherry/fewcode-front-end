1c7595d9625ecd1ee94e24b06ffe1017dad5476b
wxycherry <163128074+wxycherry@users.noreply.github.com>
2024-09-22 13:47:53 +0800

    删除大数据图标及其组件

--- src/router/route.ts
+++ src/router/route.ts
@@ -156,21 +156,6 @@ export const dynamicRoutes: Array<RouteRecordRaw> = [
 				],
 			},
 			{
-				path: '/chart',
-				name: 'chartIndex',
-				component: () => import('/@/views/chart/index.vue'),
-				meta: {
-					title: 'message.router.chartIndex',
-					isLink: '',
-					isHide: false,
-					isKeepAlive: true,
-					isAffix: false,
-					isIframe: false,
-					roles: ['admin', 'common'],
-					icon: 'iconfont icon-ico_shuju',
-				},
-			},
-			{
 				path: '/personal',
 				name: 'personal',
 				component: () => import('/@/views/personal/index.vue'),
